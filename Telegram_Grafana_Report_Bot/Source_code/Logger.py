b674b24aaabc22944e0312759e7966942fa1014f
Alexander <pawsanie@gmail.com>
2024-10-10 23:03:18 +0500

    Update 0.0.1: Docker and base python scripts

---
+++ Telegram_Grafana_Report_Bot/Source_code/Logger.py
@@ -0,0 +1,67 @@
+import logging
+from traceback import format_exc
+"""
+Logging configuration.
+"""
+
+
+def logging_config(*, log_path: str, log_level: int):
+    """
+    Get logging configuration.
+    As result set logging rules.
+    --------------
+    log_path - path to logging file.
+    log_level:
+    CRITICAL - 50
+    ERROR - 40
+    WARNING - 30
+    INFO - 20
+    DEBUG - 10
+    NOTSET - 0
+    """
+    logging.basicConfig(
+        filename=log_path,
+        encoding='utf-8',
+        level=log_level,
+        format='%(asctime)s - %(levelname)s:\n%(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S %p'
+    )
+
+
+def text_for_logging(*, log_text: str, log_error: Exception = None) -> str:
+    """
+    Wrapper for log text.
+    :param log_text: Arbitrary text for logging.
+    :type log_text: str
+    :param log_error: Custom or standard Exception object.
+    :type log_error: Exception
+    """
+    if log_error is not None:
+        log_error: str = f"\nRaise: {repr(log_error)}"
+    else:
+        log_error: str = ""
+    result_text: str = \
+        f"{'=' * 52}"\
+        f"{log_error}"\
+        f"\n{log_text}"\
+        f"\n{'-' * 52}"\
+        f"\n{format_exc()}"\
+        f"\n{'=' * 52}\n\n"
+    return result_text
+
+
+def error_logger(function):
+    """
+    Error logger.
+    """
+    def wrapper_function(*args, **kwargs):
+        try:
+            function(*args, **kwargs)
+        except Exception as error:
+            logging.error(
+                text_for_logging(
+                    log_text="Raise Exception:",
+                    log_error=error)
+            )
+        return wrapper_function
+    return function
---
+++ Telegram_Grafana_Report_Bot/Source_code/entry_point.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Shell script parse temporary config to standard configuration file for Bot pod.
+
+# Workaround for Docker's File Inode Change Issue:
+cat /application/temporary_config.json > /application/config.json
+
+# Parse Bot config:
+sed -i "s|\${TELEGRAM_CHANNEL_ID}|$TELEGRAM_CHANNEL_ID|g" /application/config.json
+sed -i "s|\${TELEGRAM_BOT_TOKEN}|$TELEGRAM_BOT_TOKEN|g" /application/config.json
+sed -i "s|\${TELEGRAM_BOT_DM_WHITE_LIST}|$TELEGRAM_BOT_DM_WHITE_LIST|g" /application/config.json
+sed -i "s|\${GRAFANA_URL}|$GRAFANA_URL|g" /application/config.json
+sed -i "s|\${GRAFANA_TOKEN}|$GRAFANA_TOKEN|g" /application/config.json
+
+# Run application:
+python -B ./Source_code/Telegram_Grafana_Report_Bot.py
